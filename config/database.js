9948c20bbe1cafee1dae180294863d18cf9a87e5
Thanachit Sengsalee <47846297+6010110455@users.noreply.github.com>
2023-02-18 22:45:07 +0700

    add database

--- config/database.js
+++ config/database.js
@@ -4,12 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true),
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
   },
